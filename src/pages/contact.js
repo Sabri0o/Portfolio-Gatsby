87b5c5fb28dab303c19daa33cf00faffebb0a11c
Sabri0o <sabritrabelsi90@gmail.com>
2021-11-28 10:09:47 +0100

    tweaking css

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,24 +1,18 @@
 import React from "react";
 import Layout from "../components/layout";
-const myStyle = {
-  marginLeft: "100px",
-  marginRight: "60px",
-  fontFamily: "Heebo",
-  fontStyle: "normal",
-  fontWeight: "normal",
-  fontSize: "20px",
-  lineHeight: "23px",
-  color: "#21243D",
-};
+import { Container } from "react-bootstrap";
+import "../styles/contact.css";
 
 export default function Contact() {
   return (
     <Layout>
-      <h2 style={{ marginLeft: "100px", marginTop: "100px" }}>Hi there ;)</h2>
-      <div style={myStyle}>
-        I'm a full stack JavaScript web developer from Tunisia. Looking forward
-        to meet you!
-      </div>
+      <Container>
+        <h2>Hi there ;)</h2>
+        <div className="myStyle">
+          I'm a full stack JavaScript web developer from Tunisia. Looking
+          forward to meet you!
+        </div>
+      </Container>
     </Layout>
   );
 }
